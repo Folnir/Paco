755eadc2743fd9ce9349138ef2303e98fb276e90
Paco <pepe@paco.com>
2019-02-11 10:20:41 +0100

    Mensaxe

---
+++ index.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
+</head>
+<body>
+<style>
+body {
+        background-image: url("welcomehome.jpg");
+} 
+ 
+</style>
+
+<div class="dropdown">
+  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    Menu
+  </button>
+  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+    <a class="dropdown-item" href="habitos.php">Hábitos</a>
+    <a class="dropdown-item" href="registro.php">Registro</a>
+    <a class="dropdown-item" href="usuario.php">Usuario</a>
+  </div>
+</div>
+
+
+</body>
+<div style="text-align:center;padding:1em 0;"> <h2><a style="text-decoration:none;" href="https://www.zeitverschiebung.net/es/city/3105976"><span style="color:gray;">Hora actual en</span><br />Vigo, España</a></h2> <iframe src="https://www.zeitverschiebung.net/clock-widget-iframe-v2?language=es&size=large&timezone=Europe%2FMadrid" width="100%" height="140" frameborder="0" seamless></iframe> </div>
+</html>
